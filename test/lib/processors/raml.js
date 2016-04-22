9d32148b56811ae5947e6509371dd4cb788e21c7
Nikita Galkin <nikita@galk.in>
2016-04-22 12:30:45 +0300

    Refactoring in raml processor

--- test/lib/processors/raml.js
+++ test/lib/processors/raml.js
@@ -8,54 +8,54 @@ var fs = require('fs');
 describe('raml', function () {
   describe('preprocess', function () {
     var preprocess = plugin.processors['.raml'].preprocess;
-    it('should return RAML', function () {
-      var fileName = makeRAMLFilePath('valid.raml');
-      var text = fs.readFileSync(fileName);
-      var result = preprocess(text, fileName);
+    it('should return the same text', function () {
+      var fileName = 'reallyLongFileName';
+      var text = 'long long text';
 
-      assert.isArray(result, 'preprocess should return array');
-      assert.strictEqual(result[0].title, 'Test');
-      assert.strictEqual(result[0].resources[0].relativeUri, '/customers');
+      var newText = preprocess(text, fileName);
+      assert.isArray(newText, 'preprocess should return array');
+      assert.strictEqual(newText[0], text);
     });
   });
   describe('postprocess', function () {
     var postprocess = plugin.processors['.raml'].postprocess;
     var preprocess = plugin.processors['.raml'].preprocess;
+    describe('raml parsing', function () {
+      it('should return no errors for valid.raml', function () {
+        var fileName = makeRAMLFilePath('valid.raml');
+        var text = fs.readFileSync(fileName);
+        preprocess(text, fileName);
+        var results = postprocess([], fileName);
 
-    it('should return no errors for valid.raml', function () {
-      var fileName = makeRAMLFilePath('valid.raml');
-      var text = fs.readFileSync(fileName);
-      preprocess(text, fileName);
-      var results = postprocess([], fileName);
+        assert.isArray(results, 'should return an array');
+        assert.lengthOf(results, 0, 'valid.raml shouldnt have any errors');
+      });
 
-      assert.isArray(results, 'should return an array');
-      assert.lengthOf(results, 0, 'valid.raml shouldnt have any errors');
-    });
+      it('should return an error for the missing include', function () {
+        var fileName = makeRAMLFilePath('missingInclude.raml');
+        var text = fs.readFileSync(fileName);
+        preprocess(text, fileName);
+        var results = postprocess([], fileName);
 
-    it('should return an error for the missing include', function () {
-      var fileName = makeRAMLFilePath('missingInclude.raml');
-      var text = fs.readFileSync(fileName);
-      preprocess(text, fileName);
-      var results = postprocess([], fileName);
+        assert.isArray(results, 'should return an array');
+        assert.lengthOf(results, 1, 'should return one error');
+        var error = results[0];
+        assert.strictEqual(error.line, 12, 'should point to twelve line');
+        assert.strictEqual(error.column, 22, 'should point to 21 character');
+      });
 
-      assert.isArray(results, 'should return an array');
-      assert.lengthOf(results, 1, 'should return one error');
-      var error = results[0];
-      assert.strictEqual(error.line, 12, 'should point to twelve line');
-      assert.strictEqual(error.column, 22, 'should point to 21 character');
-    });
+      it('should return an error for the authorizationUri Twice', function () {
+        var fileName = makeRAMLFilePath('samePropertyTwice.raml');
+        var text = fs.readFileSync(fileName);
+        preprocess(text, fileName);
+        var results = postprocess([], fileName);
 
-    it('should return an error for the authorizationUri Twice', function () {
-      var fileName = makeRAMLFilePath('samePropertyTwice.raml');
-      var text = fs.readFileSync(fileName);
-      preprocess(text, fileName);
-      var results = postprocess([], fileName);
-
-      assert.isArray(results, 'should return an array');
-      assert.lengthOf(results, 1, 'should return one error');
-      var error = results[0];
-      assert.strictEqual(error.line, 12, 'should point to twelve line');
-      assert.strictEqual(error.column, 8, 'should point to seven character');
+        assert.isArray(results, 'should return an array');
+        assert.lengthOf(results, 1, 'should return one error');
+        var error = results[0];
+        assert.strictEqual(error.line, 12, 'should point to twelve line');
+        assert.strictEqual(error.column, 8, 'should point to seven character');
+      });
     });
   });
 });
